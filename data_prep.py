5a49e01f49f0ce7e9e70a36c82fd5e192d613afd
emiliomoran <emiliojmp9@gmail.com>
2020-01-23 20:09:35 -0500

    Agregando comentarios@

--- data_prep.py
+++ data_prep.py
@@ -1,34 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 06:46:53 2019
-@author: rian-van-den-ander
-"""
-
-# ---- IMPORTS AND INPUTS -----
-
+# Importando librerías
 import numpy as np
 import pandas as pd
 from encode_json_column import encode_json_column
 from datetime import datetime
 
-# Importing the dataset
+# Leyendo los archivos de datos
 dataset = pd.read_csv('tmdb_5000_movies.csv')
 dataset_credits = pd.read_csv('tmdb_5000_credits.csv')
 dataset = pd.concat([dataset, dataset_credits], axis=1)
 
-# meaning out 0 budgets - there are a lot, so this is better than removing the rows
+# Se reemplaza los valores de presupuesto de 0 con el valor promedio de presupuesto de las películas
 dataset['budget']=dataset['budget'].replace(0,dataset['budget'].mean())
 
 X = dataset.iloc[:, :].values
 y_revenue = dataset.iloc[:, 12].values
 y_rating = dataset.iloc[:, 18].values
 
-
-# picking independent variables
 X = X[:,[0,1,4,9,11,13,14,15,22,23]]
 
-# Removing zero REVENUES from the data - revenue is super important
-# I could (and have) adjusted for inflation, but it made scant difference to model performance
+# Luego, posteriormente se remueven los valores de taquilla y presupuesto con valor 0
 y_revenue_removed = []
 y_rating_removed = []
 X_removed = []
@@ -41,21 +31,7 @@ y_revenue = np.array(y_revenue_removed)
 y_rating = np.array(y_rating_removed)
 X = np.array(X_removed)
 
-# Ajusting inflation to 2019 at average inflation - 3.22%
-# do this only if using revenue (12 y index)
-avg_inflation = 1.01322
-year_now = 2019
-for l in range(0,len(y_revenue)):
-    try:
-        film_year = int(X[l,4][0:4])
-        y_revenue[l] = y_revenue[l]*(avg_inflation ** (year_now-film_year))
-        X[l,7] = int(film_year)
-    except:
-        X[l,4] = 0
-
-# converting film date to day of year
-# i am arguably losing the 'year' which might be slightly correlated with film success
-# but that opens up a whole new can of worms about ratings and revenues by year
+# Se procedió a separar la fecha entre año y día
 for l in range(0,len(y_revenue)):
     film_date = X[l,4]
     try:
@@ -66,31 +42,24 @@ for l in range(0,len(y_revenue)):
 
 dataset =  pd.DataFrame(X)
 
-# encoding genres. 
-# using name because "id" overlaps with "id" in the next encoding, and so on
 dataset = encode_json_column(dataset, 1,"name")
 
-# encoding keywords
-# limiting to 100 codes, and removing anything not within those 100
-# yes, it is column 1 now, since last column 1 was removed by previous encoding
-dataset = encode_json_column(dataset, 1, "name", 500, 1) #was 100
+# Codificando keywords, se trabaja solo con el top 100 de las keywords
+dataset = encode_json_column(dataset, 1, "name", 500, 1)
 
-# encoding production companies.
-# limiting to 100 codes, and removing anything not within those 100
-dataset = encode_json_column(dataset, 1,"name", 500, 1) #was 100
+# Codificando compañías productoras, se trabaja solo con el top 100 de las compañías productoras
+dataset = encode_json_column(dataset, 1,"name", 500, 1)
 
-# encoding all spoken languages
+# Se codifica todos los lenguajes de las películas
 dataset = encode_json_column(dataset, 3,"iso_639_1")
 
-# encoding cast
-# encoding 'just' top 500 cast
+# Codificando elenco, se toman el top 100 del elenco
 dataset = encode_json_column(dataset, 4,"name", 500, 1) #was 500
 
-# encoding crew
-# encoding 'just' top 500 cast
+# Codificando personal, se toman el top 100 del personal
 dataset = encode_json_column(dataset, 4,"name", 500, 1) #was 500
 
-#saving to CSVs as a checkpoint to be used in regressors
+# Se guardan los archivos para usarlos en la creación del modelo
 dataset.to_csv(r'Encoded_X2.csv')
 dataset_y_revenue = pd.DataFrame(y_revenue)
 dataset_y_revenue.to_csv(r'Encoded_y - revenue2.csv')
--- film_rating_with_cast_best_regressor_colab.py
+++ film_rating_with_cast_best_regressor_colab.py
@@ -1,44 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 06:46:53 2019
-@author: rian-van-den-ander
-"""
+# Importando librerías
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-#Loading data from preprocessed CSVs
-dataset_X_reimported = pd.read_csv('https://raw.githubusercontent.com/emiliomoran/AI_Project/master/Encoded_X2.csv')
-dataset_y_reimported = pd.read_csv('https://raw.githubusercontent.com/emiliomoran/AI_Project/master/Encoded_y%20-%20rating2.csv')
+# Cargando los csv
+dataset_X_reimported = pd.read_csv('Encoded_X2.csv')
+dataset_y_reimported = pd.read_csv('Encoded_y-rating2.csv')
 dataset_reimported = pd.concat([dataset_X_reimported,dataset_y_reimported],axis=1)
 dataset_reimported = dataset_reimported.replace([np.inf, -np.inf], np.nan)
-dataset_reimported = dataset_reimported.dropna() #just two rows are lost by dropping NaN values. Better than using mean here
+dataset_reimported = dataset_reimported.dropna() # Se eliminan los valores nulos
 
 X = dataset_reimported.iloc[:, 1:-2].values
 y = dataset_reimported.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
-# I have a fairly large dataset of +- 4000 entries, so I'm going with 10% test data
+# Se divide los datos leídos, entre datos de entrenamiento y datos de prueba
 from sklearn.model_selection import train_test_split, GridSearchCV
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-#This regressor was picked with gridsearch over many parameters - took 4 hours
+# Importando la librería de xgboots
 from xgboost import XGBRegressor
 
+# Conjunto de hiper-parámetros necesarios para xgboots y entrenar nuestro modelo
 params = {'min_child_weight':[4,5], 'gamma':[i/10.0 for i in range(3,6)],  'subsample':[i/10.0 for i in range(6,11)],
 'colsample_bytree':[i/10.0 for i in range(6,11)], 'max_depth': [2,3,4], 'objective':['reg:squarederror']}
 
+# Combinación de parámetros obtenidos
+# colsample_bytree= 0.6,
+# gamma= 0.7,
+# max_depth= 4,
+# min_child_weight= 5,
+# subsample = 0.8, objective='reg:squarederror'
+
 xgb = XGBRegressor(nthread=-1)
 
+# El tiempo para encontrar los hiperparámetros y entrenar nuestro modelo es de aproximado 12 horas
 regressor = GridSearchCV(xgb, params)
-
 regressor.fit(X, y)
 
+# Usando el modelo con el dato de entrenamiento, para obtener predicción
 y_pred = regressor.predict(X_test)
 
-
+# Calculando métricas de evaluación de nuestro modelo
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 score = r2_score(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
@@ -50,12 +53,13 @@ print("MAE:", mae)
 print("MSE:", mse)
 print("--------------------------")
 
+# Generando gráfico de Measured Rating vs Predicted Rating
 fig, ax = plt.subplots()
 ax.scatter(y_test, y_pred)
 ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
 ax.set_xlabel('Measured rating')
 ax.set_ylabel('Predicted rating')
 plt.title('Measured versus predicted rating')
-plt.ylim((5, 9))   # set the ylim to bottom, top
-plt.xlim(5, 9)     # set the ylim to bottom, top
+plt.ylim((5, 9))
+plt.xlim(5, 9)
 plt.show()
